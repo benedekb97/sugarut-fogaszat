97cd7b00c91c360e7398616d6d8a8c8ec997f6f9
Benedek Burgess <benedekb97@sch.bme.hu>
2024-06-06 15:16:15 +0100

    Add SEO tags

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,6 +3,13 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="title" content="Sugárút Fogászat Debrecen">
+        <meta name="description" content="Megnyugtató, barátságos környezetben várjuk betegeinket, Debrecen központjában lévő magánrendelőnkben.">
+        <meta name="keywords" content="fogászat,fogorvos,debrecen,fogpótlás,gyökérkezelés,fogszabályozás,fogszabályzó,protézis">
+        <meta name="robots" content="index, follow">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="revisit-after" content="10 days">
+        <meta name="author" content="Burgess Benedek">
 
         <title>Sugárút Fogászat @yield('title')</title>
         @vite(['resources/css/app.scss'])
