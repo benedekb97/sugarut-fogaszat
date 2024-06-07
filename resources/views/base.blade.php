020e9178344e1fb75eb0c429eee3fc1195602c81
Benedek Burgess <benedekb97@sch.bme.hu>
2024-06-07 10:01:38 +0100

    Use html special chars in meta tags

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="title" content="Sugárút Fogászat Debrecen">
-        <meta name="description" content="Megnyugtató, barátságos környezetben várjuk betegeinket, Debrecen központjában lévő magánrendelőnkben.">
-        <meta name="keywords" content="sugárút,fogászat,fogorvos,debrecen,fogpótlás,gyökérkezelés,fogszabályozás,fogszabályzó,protézis">
+        <meta name="title" content="Sug&aacute;r&uacute;t Fog&aacute;szat Debrecen">
+        <meta name="description" content="Megnyugtat&oacute;, bar&aacute;ts&aacute;gos k&ouml;rnyezetben v&aacute;rjuk betegeinket, Debrecen k&ouml;zpontjában l&eacute;v&odblac; mag&aacute;nrendel&odblac;nkben.">
+        <meta name="keywords" content="sug&aacute;r&uacute;t,fog&aacute;szat,fogorvos,debrecen,fogp&oacute;tl&aacute;s,gy&ouml;k&eacute;rkezel&eacute;s,fogszab&aacute;lyoz&aacute;s,fogszab&aacute;lyz&oacute;,prot&eacute;zis">
         <meta name="robots" content="index, follow">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="revisit-after" content="10 days">
